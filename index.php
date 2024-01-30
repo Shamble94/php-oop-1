44e0ad054e30dcb4e465fc0075a480c24ee2b2cb
Shamble94 <fratus.nicholas@gmail.com>
2024-01-30 16:37:03 +0100

    Bonus 2 completato

--- index.php
+++ index.php
@@ -25,32 +25,26 @@ class Movies{
 $movie_1 = new Movies("Il signore degli anelli: la compagnia dell'anello", "Fantascienza", "Si");
 $movie_2 = new Movies ("Il signore degli anelli: le due torri", "Fantascienza", "Si");
 $movie_3 = new Movies ("Il signore degli anelli: il ritorno del re", "Fantascienza", "Si");
-$movie_4 = new Movies ("Lo hobbit: la desolazione di Smaug", "Fantascienza", "No");
-$movie_5 = new Movies ("Lo hobbit: la battaglia delle 5 armate", "Fantascienza", "No");
+$movie_4 = new Movies ("Spiral", "Horror", "No");
+$movie_5 = new Movies ("Scream V", "Horror", "No");
 
-echo  "<div>".$movie_1->title. "  " .$movie_1->seenMovie($movie_1->seen)."</div> <br>";
-echo "<div>".$movie_2->title. "  " .$movie_2->seenMovie($movie_2->seen)."</div> <br>";
-echo "<div>".$movie_3->title. "  " .$movie_3->seenMovie($movie_3->seen)."</div> <br>";
-echo "<div>".$movie_4->title. "  " .$movie_4->seenMovie($movie_4->seen)."</div> <br>";
-echo "<div>".$movie_5->title. "  " .$movie_5->seenMovie($movie_5->seen)."</div> <br>";
+
+$movies = [
+    $movie_1,
+    $movie_2,
+    $movie_3,
+    $movie_4,
+    $movie_5,
+    
+];
+
+echo "Lista Film";
+foreach ($movies as $movie){
+    echo
+    "<ul>" ."<li>". "Titolo:". " " .$movie->title ."<br>". "Genere:". " " .$movie->genre ."<br>" ."Hai visto questo film?:". " " .$movie->seen ."<br>" ."</li>" ."</ul>";
+    ;
+}
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 <!DOCTYPE html>
